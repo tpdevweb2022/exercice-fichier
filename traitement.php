e43e05fff25e1b7ef983d3515ce448fa8379db42
anthonymerlier <webmaster@consor.fr>
2022-05-11 09:26:56 +0200

    Add error text per condition

--- traitement.php
+++ traitement.php
@@ -1,17 +1,19 @@
 <?php
 
-var_dump($_FILES);
+// var_dump($_FILES);
 
 // On vérifie si c'est bien une méthode POST
-var_dump($_SERVER["REQUEST_METHOD"]);
-var_dump($_POST);
+// var_dump($_SERVER["REQUEST_METHOD"]);
+// var_dump($_POST);
 if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["submit_btn"])) {
     // ... On continue
     if ($_FILES["fichier"]["error"] !== 0) {
+        echo "Une erreur fatale est survenue";
         die; // On stoppe
     }
     $sizeMax = 2 * 1_048_576; // 2 Mo
     if ($_FILES["fichier"]["size"] > $sizeMax) {
+        echo "Fichier trop volumineux";
         die; // On stoppe
     }
     // Extensions qu'on souhaite autorisée
@@ -25,13 +27,16 @@ if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["submit_btn"])) {
     $extension = strtolower(end($params)); // jpg, png, ...
     // On va vérifier si l'extension est autorisée
     if (!in_array($extension, $authorizedExt)) {
+        echo "Extension non autorisée";
         die;
     }
 
     $filename = time() . bin2hex(random_bytes(48)) . "." . $extension;
 
-    move_uploaded_file(
+    $status = move_uploaded_file(
         $_FILES["fichier"]["tmp_name"],
         "uploads/" . $filename
     );
+    echo "Uploadé";
+    var_dump($status);
 }
